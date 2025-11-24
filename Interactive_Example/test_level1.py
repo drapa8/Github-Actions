86e708a23ce1cc3c5fab40cc7c13484cf9820fcb
Luis Castillo <luiseduardocastilloruvalcaba@gmail.com>
2025-11-23 19:57:17 -0600

    level 1

---
+++ Interactive_Example/test_level1.py
@@ -0,0 +1,12 @@
+import pytest
+from calculator import divide
+
+def test_divide_basic():
+    assert divide(10, 2) == 5
+
+def test_divide_zero():
+    try:
+        resultado = divide(10, 0)
+        assert resultado is None
+    except ZeroDivisionError:
+        pytest.fail()
